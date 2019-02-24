82628224ff99d5d5f5b30b8d34d6c0dc685ecae7
Dayo Akinkuowo <me@sprypixels.com>
2019-02-24 01:26:32 +0100

    Updates

---
+++ precache-manifest.6c4fffdcb9482bb8306b0087f6c368d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b78dc987ff1089580417de17b7cf9dce",
+    "url": "/ChinguBookFinder/static/media/loading.b78dc987.svg"
+  },
+  {
+    "revision": "e85191c64ea3472797e55dad0e72d7b1",
+    "url": "/ChinguBookFinder/static/media/cancel.e85191c6.svg"
+  },
+  {
+    "revision": "050de0b8468e593124a0",
+    "url": "/ChinguBookFinder/static/js/runtime~main.050de0b8.js"
+  },
+  {
+    "revision": "c09cad464dd2fbfe07a7",
+    "url": "/ChinguBookFinder/static/js/main.c09cad46.chunk.js"
+  },
+  {
+    "revision": "e2a032349bc2797c4db5",
+    "url": "/ChinguBookFinder/static/js/2.e2a03234.chunk.js"
+  },
+  {
+    "revision": "c09cad464dd2fbfe07a7",
+    "url": "/ChinguBookFinder/static/css/main.32f5f6e7.chunk.css"
+  },
+  {
+    "revision": "01356ac8846f2a9885928c263bb45172",
+    "url": "/ChinguBookFinder/index.html"
+  }
+];
